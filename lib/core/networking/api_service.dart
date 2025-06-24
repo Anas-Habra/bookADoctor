485d0f4b66cf50c29f283b8b2408ab92279cca92
Anas Habra <anasha1420@gmail.com>
2025-06-24 11:17:11 +0300

    - Signup UI & Api

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -4,6 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
 import '../../features/login/data/models/login_request_body.dart';
+import '../../features/sign_up/data/models/sign_up_request_body.dart';
+import '../../features/sign_up/data/models/sign_up_response.dart';
 
 part 'api_service.g.dart';
 
@@ -13,4 +15,7 @@ abstract class ApiService {
 
   @POST(ApiConstants.login)
   Future<LoginResponse> login(@Body() LoginRequestBody loginRequestBody);
+
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(@Body() SignupRequestBody signupRequestBody);
 }
